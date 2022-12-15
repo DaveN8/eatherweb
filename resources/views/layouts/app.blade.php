1ef155ffe20bfab395bffe319f58df8fa9185571
DaveN8 <100126795+DaveN8@users.noreply.github.com>
2022-12-15 13:13:49 +0700

    Master test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,6 +34,7 @@
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             @yield('main_content')
         </div>
+        o
 
         {{-- <!-- Page Content -->
             <main>
