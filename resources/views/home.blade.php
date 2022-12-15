5a38291053640a4a969db4e1a865859668f200a2
YuppyItuEnak <fmuhammad05@Student.ciputra.ac.id>
2022-12-15 11:30:27 +0700

    masyaallah

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         expedita, ut vero quam consectetur quisquam commodi?
     </h1> --}}
 
-    <header class="masthead">
+    {{-- <header class="masthead">
         <div class="container px-5">
             <div class="row gx-5 align-items-center">
                 <div class="col-lg-6">
@@ -24,6 +24,31 @@
                 {{-- <div class="col-lg-6">
                     
                 </div> --}}
+    {{-- </div>
+        </div>
+    </header> --}}
+
+    <header class="bg-white dark:bg-gray-900">
+        <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+            <div class="mr-auto place-self-center lg:col-span-7">
+                <h1
+                    class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
+                    Eather, <br>Teman untuk Nyemil.</h1>
+                <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Eather
+                    merupakan sebuah makanan ringan yang renyah dan enak</p>
+                <div class="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+                    <a href="#"
+                        class="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+                <img src="" alt="hero image">
             </div>
         </div>
     </header>
--- resources/views/original.blade.php
+++ resources/views/original.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.main_layout')
 
 @section('main_content')
-    <div class="container ">
+    {{-- <div class="container ">
         <div class="row">
             <div class="col">
                 <div class="card">
@@ -11,7 +11,7 @@
                             <span class="card-title fs-1">Rp. 15.000</span><span>/pcs</span>
                         </div>
                     </div>
-                    
+
                 </div>
             </div>
             <div class="col">
@@ -28,4 +28,34 @@
                 </div>
             </div>
         </div>
-    @endsection
+    </div> --}}
+
+
+
+    <div class="grid grid-cols-3 gap-4">
+        <div class="col-start-2 col-span-4">
+            <div
+                class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                <div class="p-5">
+                    <div>
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Rp.
+                            15.000<span>/pcs</span></h5>
+                    </div>
+                </div>
+                <div>
+                    <img class="rounded-t-lg" src="" alt="" />
+                </div>
+            </div>
+        </div>
+        <div class="col-span-2 ">
+            <h1 class="text-center">Original</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed erat facilisis, lobortis nisl et,
+                    fermentum mauris. Aliquam vel mi ut erat hendrerit suscipit. Pellentesque vel pretium sapien.
+                    Aliquam facilisis, lorem vitae aliquet imperdiet, nunc justo tristique lectus,
+                    mollis posuere nulla nibh a erat. Ut aliquet sapien vulputate, dapibus arcu vel, interdum dolor.
+                    Maecenas ac dui vitae dui vestibulum semper. Etiam convallis nunc id consequat mollis.
+                    Pellentesque rutrum sagittis sodales. Praesent suscipit, risus vitae malesuada bibendum,
+                    ipsum diam iaculis orci, ut convallis sem ligula ac dui.</p>
+        </div>
+    </div>
+@endsection
