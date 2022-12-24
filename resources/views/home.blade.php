bafa5f770cfeccdc8213c1fee236c11c04360bc3
DaveN8 <100126795+DaveN8@users.noreply.github.com>
2022-12-25 01:31:56 +0700

    tambahan dikit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,8 +45,8 @@
                 </a>
             </div>
             <!--Right Col-->
-            <div class="w-full md:w-3/5 py-6 text-center">
-                <img class="w-full md:w-4/5 z-50" src="image/posterhome.png" />
+            <div class="flex justify-center w-full md:w-3/5 py-6 text-center">
+                <img class="w-full md:w-4/5 z-50 rounded rounded-lg" src="image/posterhome.png" />
             </div>
         </div>
     </div>
